fc326df81dd05fb3baca32da182c7e49dda13996
shreyagoti09 <spgoti09@gmail.com>
2021-03-03 13:02:43 +0530

    fashionerMain

--- Fashioner/Management/models.py
+++ Fashioner/Management/models.py
@@ -1,18 +1,13 @@
 from django.db import models
 from django.conf import settings
 
-CATAGORY_CHOISES={
-    ('S','Shirt'),
-    ('SW','Sports wear'),
-    ('OW','outwear'),
-}
 # Create your models here.
 #Items class
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField(default=0)
     image = models.CharField(max_length = 500, default="")
-    category = models.CharField(choices=CATAGORY_CHOISES,max_length=3)
+    category = models.CharField(max_length=100, default="")
     def __str__(self):
         return self.title
 
--- Fashioner/fashionerMain/apps.py
+++ Fashioner/fashionerMain/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class FashionermainConfig(AppConfig):
-    name = 'fashionerMain'
+    name = 'FashionerMain'
