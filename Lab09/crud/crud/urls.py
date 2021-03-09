675c0be381b714836169b34c507c957337db6d23
Sweta Kyada <swetakyada20@gmail.com>
2021-03-09 14:49:21 +0530

    Lab09

---
+++ Lab09/crud/crud/urls.py
@@ -0,0 +1,28 @@
+"""crud URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path 
+from crudapp import views  
+from django.conf.urls import url
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.index),
+    url(r'^create$', views.create, name='create'),
+    url(r'^edit/(?P<id>\d+)$', views.edit, name='edit'),
+    url(r'^edit/update/(?P<id>\d+)$', views.update, name='update'),
+    url(r'^delete/(?P<id>\d+)$', views.delete, name='delete'),
+]
---
+++ Lab09/crud/crudapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+ 
+# Create your models here.
+class Member(models.Model):
+    firstname = models.CharField(max_length=40)
+    lastname = models.CharField(max_length=40)
+     
+    created = models.DateTimeField(auto_now_add=True)
+     
+    def __str__(self):
+        return self.firstname + " " + self.lastname
+         
+    class Meta:  
+        db_table = "blog_member"
