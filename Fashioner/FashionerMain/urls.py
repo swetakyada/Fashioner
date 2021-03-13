d1933e02c375eb5e9b8d687b761e2dd9c76ebecb
shreyagoti09 <spgoti09@gmail.com>
2021-03-13 22:12:51 +0530

    checkout product-detail wishlist logout created

--- Fashioner/FashionerMain/urls.py
+++ Fashioner/FashionerMain/urls.py
@@ -3,8 +3,10 @@ from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
-    path('', views.products, name='home'),
-    path('checkout/', TemplateView.as_view(template_name='checkout-page.html'), name='checkout'),
-    path('products/', TemplateView.as_view(template_name='product-page.html'), name='products'),
-    path('cart/', TemplateView.as_view(template_name='cart-page.html'), name='cart'),
+    path('home/', views.products, name='home'),
+    path('home/logout',TemplateView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('home/checkout/', TemplateView.as_view(template_name='checkout-page.html'), name='checkout'),
+    path('home/products/', TemplateView.as_view(template_name='product-page.html'), name='products'),
+    path('home/cart/', TemplateView.as_view(template_name='cart-page.html'), name='cart'),
+    path('home/wishlist/', TemplateView.as_view(template_name='wishlist-page.html'), name='wishlist'),
 ]
