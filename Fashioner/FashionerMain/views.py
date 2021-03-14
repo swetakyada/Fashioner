59e0f40e931aabe3aaa1a7f10b5f9307d66096a6
shreyagoti09 <spgoti09@gmail.com>
2021-03-14 19:21:46 +0530

    profile,category pages added and some changes made

--- Fashioner/FashionerMain/views.py
+++ Fashioner/FashionerMain/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Product,Cart
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required(login_url='/accounts/')
 def products(request):
     products = Product.objects.all()
     context = {
@@ -12,7 +14,7 @@ def products(request):
 # class HomeView(ListView):
 #     model=Item
 #     template_name="home-page.html"
-
+@login_required(login_url='/accounts/')
 def product_page(request, pid):
     product = Product.objects.get(id=pid)
     return render(request, "product-page.html", {'product': product})
