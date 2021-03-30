9d3c0a74895e099859eb97a9030e83b830969fc7
Shreya Goti <66583470+shreyagoti09@users.noreply.github.com>
2021-03-30 11:27:09 +0530

    test.py updated

--- Fashioner/accounts/tests.py
+++ Fashioner/accounts/tests.py
@@ -21,27 +21,20 @@ class AccountTestCase(LiveServerTestCase):
     def test_register(self):
         selenium = self.selenium
         #Opening the link we want to test
-        selenium.get('http://127.0.0.1:8000/accounts/signup/')
+        selenium.get('http://127.0.0.1:8000/accounts/login/')
         #find the form element
-        first_name = selenium.find_element_by_name('first_name')
-        last_name = selenium.find_element_by_name('last_name')
         username = selenium.find_element_by_name('username')
-        email = selenium.find_element_by_name('email')
-        password1 = selenium.find_element_by_name('password1')
-        password2 = selenium.find_element_by_name('password2')
-        submit = selenium.find_element_by_name('signup')
+        password = selenium.find_element_by_name('password')
+        submit=selenium.find_element_by_name('submit')
 
         #Fill the form with data
-        username.send_keys('unary')
-        first_name.send_keys('Yusuf')
-        last_name.send_keys('Unary')
-        email.send_keys('contact@yusuf.im')
-        password1.send_keys('123456')
-        password2.send_keys('123456')
+        username.send_keys('fashion')
+        password.send_keys('fashion')
+        
         time.sleep(3000)
         #submitting the form
         submit.send_keys(Keys.RETURN)
 
         #check the returned result
-        assert email.text == 'contact@yusuf.im'
+        
         # assert 'Check your email' in selenium.page_source
